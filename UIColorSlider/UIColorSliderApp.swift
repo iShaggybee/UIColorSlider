2cedc9a9eb13302bb8664e98284cfd4e84b38a79
Vadim Kislov <kislov.v@gmail.com>
2022-07-17 13:08:44 +0300

    Initial Commit

---
+++ UIColorSlider/UIColorSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIColorSliderApp.swift
+//  UIColorSlider
+//
+//  Created by Kislov Vadim on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIColorSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
